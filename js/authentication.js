971717e184e9a819b68cd066e900715a6b855dee
Chadwick Flores <chadwickflores@gmail.com>
2016-07-05 10:46:03 -0500

    add authentication

--- js/authentication.js
+++ js/authentication.js
@@ -26,8 +26,78 @@ authApp.controller('authCtrl', ['$scope', '$http', '$location', '$state', 'rails
        });
    }
 
+   $scope.loginUser = function(user_email, user_password) {
+       var credentials = {
+           user: {
+               email: user_email,
+               password: user_password
+           }
+       };
+       $http({
+           method: 'POST',
+           url: rails_server_path + '/users/sign_in.json',
+           data: credentials
+       }).then(function successCallback(response) {
+           console.log(response.data);
+           // save email and token in local storage
+           var authentication_token = response.data.authentication_token;
+           var email = response.data.user.email;
+           StoreLoginData(authentication_token, email);
+           // shows dashboard
+           $state.go("dashboard");
+       }, function errorCallback(error) {
+           $scope.login_status = "alert alert-danger";
+           $scope.login_message = "Invalid email or password";
+           fadeAlert("#login_alert");
+       });
+   }
+
+   $scope.forgotPassword = function(user_email) {
+       $http({
+           method: 'GET',
+           url: rails_server_path + '/send_password.json?user_email=' + user_email
+       }).then(function successCallback(response) {
+           $scope.forgot_status = "alert alert-success";
+           $scope.forgot_message = "Please check your inbox";
+           fadeAlert("#forgot_alert");
+       }, function errorCallback(error) {
+           $scope.forgot_status = "alert alert-danger";
+           $scope.forgot_message = "Unable to send recovery email";
+           fadeAlert("#forgot_alert");
+       });
+   }
+
+   $scope.resetPassword = function(new_password, confirm_new_password) {
+       var reset_token = $location.search().reset_password_token;
+       $http({
+           method: 'PUT',
+           url: rails_server_path + '/users/password.json',
+           data: {
+               user: {
+                   password: new_password,
+                   confirm_password: confirm_new_password,
+                   reset_password_token: reset_token
+               }
+           }
+       }).then(function successCallback(response) {
+           $location.search('reset_password_token', null);
+           $state.go("login");
+       }, function errorCallback(error) {
+           $scope.reset_status = "alert alert-danger";
+           $scope.reset_message = "Unable to reset password";
+           fadeAlert("#reset_alert");
+       });
+   }
+
    function fadeAlert(id) {
        $(id).fadeTo(3000, 0);
    }
-   
+
 }]);
+
+authApp.factory('StoreLoginData', function() {
+  return function(authentication_token, email) {
+    localStorage.token = authentication_token;
+    localStorage.email = email;
+  }
+});
